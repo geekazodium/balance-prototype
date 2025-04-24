540d555378b4891aae4c4596651affa6b6cfe753
HiddenAlice <ap1330167@gmail.com>
2025-04-24 23:50:54 +0800

    Updated level stuff uhh
    updated level stuff uhh

--- scripts/player/projectile/player_projectile_pool.gd
+++ scripts/player/projectile/player_projectile_pool.gd
@@ -3,7 +3,6 @@ class_name PlayerProjectilePool
 
 @export var launch_action: String = "";
 @export var projectile_source: PlatformerCharacterBody;
-@export var projectile: PlayerProjectile;
 @export var fully_charged_multiplier: float;
 @export var projectile_offset: Vector2;
 @export var player_momentum_percentage: float;
@@ -15,6 +14,10 @@ class_name PlayerProjectilePool
 
 @export var aim_stick_deadzone: float = 0.1;
 
+@export var projectile_scene: PackedScene;
+
+@export var projectiles: Array[PlayerProjectile];
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed(self.launch_action) || Input.is_action_just_released(self.launch_action):
 		if self.hold_timer > 0:
@@ -23,7 +26,7 @@ func _physics_process(delta: float) -> void:
 		
 		# This method call was sponsored my ikea (THIS IS A JOKE, DO NOT SUE ME)
 		self.aim_indicator.update_predicted_points(
-			self.projectile,
+			self.projectiles[0],
 			self.get_aim_direction(),
 			self.projectile_source, 
 			self.player_momentum_percentage,
@@ -39,8 +42,21 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed(self.launch_action):
 		self.hold_timer = self.max_hold_timer;
 
+func get_availiable_projectile() -> PlayerProjectile:
+	var i: int = 0;
+	for projectile in self.projectiles:
+		if !projectile.enabled:
+			break;
+		i += 1;
+	if i == self.projectiles.size():
+		var inst: Node = self.projectile_scene.instantiate();
+		self.add_child(inst);
+		self.projectiles.push_back(inst);
+	return self.projectiles[i];
+
 func launch_projectile() -> void:
-	self.projectile.launch(
+	var projectile: PlayerProjectile = self.get_availiable_projectile();
+	projectile.launch(
 		self.get_aim_direction(), 
 		projectile_source.global_position + self.projectile_offset,
 		fully_charged_multiplier * self.get_charge_percentage() + 1
@@ -48,7 +64,7 @@ func launch_projectile() -> void:
 	
 	self.hold_timer = self.max_hold_timer;
 	
-	self.projectile.velocity += self.projectile_source.velocity * self.player_momentum_percentage;
+	projectile.velocity += self.projectile_source.velocity * self.player_momentum_percentage;
 	EventBus.player_projectile_launch.emit();
 
 func get_aim_direction() -> Vector2:
